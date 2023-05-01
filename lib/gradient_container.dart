0d01f7dfcebda40e092a7e9311e18a74a42a2559
Alex P <peters@ap-data.de>
2023-05-01 22:48:58 +0200

    variable test

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -3,20 +3,23 @@ import 'styled_text.dart';
 
 // Dateiname immer Bezeichnungen mit _ und die Klassen darin ohne _
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Color.fromARGB(255, 26, 2, 80),
             Color.fromARGB(255, 45, 7, 98),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
