6869055b28d793ae95f7adc2b0e39b6058da033a
Alex P <peters@ap-data.de>
2023-05-04 22:18:14 +0200

    Varianten Vergleich

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -7,20 +7,17 @@ var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.startColor, this.endColor, {super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
 
-  final Color startColor;
-  final Color endColor;
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            startColor,
-            endColor,
-          ],
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
@@ -31,3 +28,25 @@ class GradientContainer extends StatelessWidget {
     );
   }
 }
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('Placed Text APX!'),
+//       ),
+//     );
+//   }
+// }
