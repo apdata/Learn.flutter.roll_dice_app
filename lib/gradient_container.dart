b169ee2671b5035e0a1937145919007e9129bfb0
Alex P <peters@ap-data.de>
2023-05-08 16:33:16 +0200

    Würfelgrafiken

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -9,6 +9,11 @@ var endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
 
+  // Multiple Constructor
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
   final Color color1;
   final Color color2;
 
@@ -22,8 +27,11 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Placed Text APX!'),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-1.png',
+          width: 200,
+        ),
       ),
     );
   }
