362f8b9cdd4094598efba06e8d69ab17783382b0
Alex P <peters@ap-data.de>
2023-05-08 22:27:18 +0200

    .

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'styled_text.dart';
 
 // Dateiname immer Bezeichnungen mit _ und die Klassen darin ohne _
 
@@ -7,15 +6,23 @@ var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1, this.color2, {super.key});
+  GradientContainer(this.color1, this.color2, {super.key});
 
   // Multiple Constructor
-  const GradientContainer.purple({super.key})
+  GradientContainer.purple({super.key})
       : color1 = Colors.deepPurple,
         color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
+
+  var activeDiceImage = 'assets/images/dice-1.png';
+
+  void rollDice() {
+    // ...
+    activeDiceImage = 'assets/images/dice-4.png';
+    print('Changing image...');
+  }
 
   @override
   Widget build(context) {
@@ -28,9 +35,31 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Image.asset(
-          'assets/images/dice-1.png',
-          width: 200,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              activeDiceImage,
+              width: 200,
+            ),
+            // Alternative zum Padding
+            // const SizedBox(
+            //   height: 20,
+            // ),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                ),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+              child: const Text('Roll Dice'),
+            ),
+          ],
         ),
       ),
     );
